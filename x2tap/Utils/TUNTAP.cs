17e4437f782e806213f817f89c0ffd977cd85078
Holli_Freed <admin@hacking001.com>
2019-03-18 10:55:21 +0800

    增加注释

--- x2tap/Utils/TUNTAP.cs
+++ x2tap/Utils/TUNTAP.cs
@@ -9,6 +9,10 @@ namespace x2tap.Utils
         public static string NETWORK_KEY = "SYSTEM\\CurrentControlSet\\Control\\Network\\{4D36E972-E325-11CE-BFC1-08002BE10318}";
         public static string ADAPTER_KEY = "SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}";
 
+        /// <summary>
+        /// 获取 TUN/TAP 适配器 ID
+        /// </summary>
+        /// <returns>适配器 ID</returns>
         public static string GetComponentId()
         {
             var adaptersRegistry = Registry.LocalMachine.OpenSubKey(ADAPTER_KEY);
@@ -25,6 +29,11 @@ namespace x2tap.Utils
             return "";
         }
 
+        /// <summary>
+        /// 获取 TUN/TAP 适配器名称
+        /// </summary>
+        /// <param name="componentId">适配器 ID</param>
+        /// <returns>适配器名称</returns>
         public static string GetName(string componentId)
         {
             var registry = Registry.LocalMachine.OpenSubKey(string.Format("{0}\\{1}\\Connection", NETWORK_KEY, componentId));
